4dd39f48d9d355c9b6c40f43248a2a50d2e79e08
Louis Henri Franc <louis.franc@insa-rouen.fr>
2017-07-23 05:17:24 -0700

    remove transformer original file

--- config.py
+++ config.py
@@ -1,18 +1,1 @@
-from types import SimpleNamespace
-
-
-class Config(SimpleNamespace):
-    """
-    Basic config class.  Loads a json and presents as an object structure
-    """
-
-    def __init__(self, data):
-        """
-        args:
-            filename - the json file to load
-        """
-        super().__init__()
-        self.__dict__.update(data.__dict__)
-
-    def __setattr__(self, key, value):
-        raise TypeError("Objects of type Config are immutable")
+ 
