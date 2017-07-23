ded60d8c332a76e088df36e7ef0a08ae96f711db
Louis Henri Franc <louis@trueai.io>
2017-07-23 20:44:53 +0000

    modify multihead tests

--- modules/core/multihead_attention.py
+++ modules/core/multihead_attention.py
@@ -3,7 +3,7 @@ import sonnet as snt
 
 
 class MultiHeadAttention(snt.AbstractModule):
-    def __init__(self, num_heads, dropout_rate, mask_leftward_decoder):
+    def __init__(self, num_heads, dropout_rate=0.0, mask_leftward_decoder=False):
         super(MultiHeadAttention, self).__init__(name="multihead_attention")
 
         self.num_heads = num_heads
@@ -15,15 +15,17 @@ class MultiHeadAttention(snt.AbstractModule):
         mask *= -2 ** 30
         mask = tf.expand_dims(tf.expand_dims(mask, 1), -1)
         mask = tf.tile(mask, [1, self.num_heads, 1, 1])
+        mask = tf.transpose(mask, [0, 1, 3, 2])
         return mask
 
     def create_mask_for_queries(self, tensor):
         mask = tf.to_float(tf.not_equal(tf.reduce_sum(tensor, axis=-1), 0.0))
+        mask = tf.expand_dims(tf.expand_dims(mask, 1), -1)
         mask = tf.tile(mask, [1, self.num_heads, 1, 1])
         return mask
 
     def create_mask_for_decoding(self, tensor):
-        masking_leftward = 1 - tf.contrib.linalg.LinearOperatorTriL(tf.ones_like(tensor[0, 0]))
+        masking_leftward = 1 - tf.contrib.linalg.LinearOperatorTriL(tf.ones_like(tensor[0, 0, :, :])).to_dense()
         masking_leftward = tf.expand_dims(tf.expand_dims(masking_leftward, 0), 0)
         masking_leftward = tf.tile(masking_leftward,
                                    [tensor.get_shape().as_list()[0], self.num_heads, 1, 1])
@@ -34,7 +36,8 @@ class MultiHeadAttention(snt.AbstractModule):
         if values is None:
             values = keys
 
-        num_outputs = keys.get_shape().as_list()[-1]
+        import pytest; pytest.set_trace()
+        num_outputs = queries.get_shape().as_list()[-1]
         q_w = tf.contrib.layers.fully_connected(queries, num_outputs)  # batch_size x query_l x d_model
         k_w = tf.contrib.layers.fully_connected(keys, num_outputs)  # batch_size x keys_l x d_model
         v_w = tf.contrib.layers.fully_connected(values, num_outputs)  # batch_size x values_l x d_model
@@ -63,8 +66,9 @@ class MultiHeadAttention(snt.AbstractModule):
         softmax_q_wi_k_wi = tf.layers.dropout(softmax_q_wi_k_wi, self.dropout_rate, is_training)
 
         attention_qwi_kwi = tf.matmul(softmax_q_wi_k_wi, v_wi)
-        attention_qwi_kwi = tf.reshape(attention_qwi_kwi, [0, 2, 3, 1])
-        concat_attention = tf.concat(attention_qwi_kwi, axis=-1)
+        attention_qwi_kwi = tf.transpose(attention_qwi_kwi, [0, 2, 3, 1])
+
+        concat_attention = tf.reshape(attention_qwi_kwi, [-1, queries.get_shape().as_list()[1], num_outputs])
 
         multi_attention = tf.contrib.layers.fully_connected(concat_attention, num_outputs)
         return multi_attention
--- modules/core/tests/test_multihead_attention.py
+++ modules/core/tests/test_multihead_attention.py
@@ -6,9 +6,9 @@ import numpy as np
 class TestMultiHeadAttention(tf.test.TestCase):
     def setUp(self):
         super(TestMultiHeadAttention, self).setUp()
-        self.num_heads = 4
+        self.num_heads = 2
         self.module = MultiHeadAttention(
-            num_heads=4,
+            num_heads=self.num_heads,
             dropout_rate=0.0,
             mask_leftward_decoder=False
         )
@@ -22,16 +22,101 @@ class TestMultiHeadAttention(tf.test.TestCase):
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         ], dtype=np.float32)
         out = self.module.create_mask_for_keys(keys)
-        with self.test_session() as sess:
+        with self.test_session():
             res = out.eval()
             self.assertEqual(res.shape, (keys.shape[0],
                                          self.num_heads,
-                                         keys.shape[1],
-                                         1))
+                                         1,
+                                         keys.shape[1]
+                                         ))
 
             self.assertAllClose(res[2, :, :, :], np.full(
-                (self.num_heads, keys.shape[1], 1),  -2 ** 30))
+                (self.num_heads, 1, keys.shape[1]),  -2 ** 30))
+            self.assertAllClose(res[0, :, :, 0], res[1, :, :, 0])
+            self.assertTrue(all(res[0, :, :, 0] != res[1, :, :, 1]))
+
+    def test_create_mask_for_queries(self):
+        queries = np.array([
+            [[1, 1, 1], [1, 1, 1], [0, 0, 0]],
+            [[1, 1, 1], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        ], dtype=np.float32)
+
+        out = self.module.create_mask_for_queries(queries)
+        with self.test_session():
+            res = out.eval()
+
+            self.assertEqual(res.shape, (queries.shape[0],
+                                         self.num_heads,
+                                         queries.shape[1],
+                                         1))
+
+            self.assertAllEqual(res[2, :, :, :], np.zeros(
+                (self.num_heads, queries.shape[1], 1)))
             self.assertAllClose(res[0, :, 0, :], res[1, :, 0, :])
             self.assertTrue(all(res[0, :, 0, :] != res[1, :, 1, :]))
 
+    def test_create_mask_for_decoding(self):
+        batch_size = 3
+        seq_len_keys = 4
+        seq_len_queries = 4  # queries = key when mask_for_decoding is used
+        tensor = tf.random_uniform(
+            (batch_size, self.num_heads, seq_len_queries, seq_len_keys))
 
+        out = self.module.create_mask_for_decoding(tensor)
+        with self.test_session():
+            res = out.eval()
+
+            self.assertEqual(res.shape, (batch_size,
+                                         self.num_heads,
+                                         seq_len_queries,
+                                         seq_len_keys))
+            for bs in range(batch_size):
+                for n_h in range(self.num_heads):
+                    for i in range(seq_len_queries):
+                        self.assertTrue(all(res[bs, n_h, i, :i+1] == 0))
+                        self.assertTrue(all(res[bs, n_h, i, i+1:] != 0))
+
+    def test_build_queries_equal_keys(self):
+        queries = np.array([
+            [[1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 0, 0]],
+            [[1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1]],
+            [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 0]]
+        ], dtype=np.float32)
+
+        queries = tf.convert_to_tensor(queries)
+        keys = queries
+        out = self.module(queries=queries,
+                          keys=keys)
+
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            res = out.eval()
+
+            self.assertEqual(res.shape, queries.shape)
+
+
+    def test_build_queries_different_keys(self):
+        queries = np.array([
+            [[1, 1, 1, 1], [1, 1, 1, 0], [0, 0, 0, 0]],
+            [[1, 1, 0, 1], [1, 0, 0, 1], [1, 0, 0, 1]]
+        ], dtype=np.float32)
+
+        keys = np.array([
+            [[1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 1], [1, 0, 0, 1]],
+            [[1, 1, 0, 1], [1, 1, 0, 1], [0, 0, 1, 0], [0, 0, 1, 1]],
+        ], dtype=np.float32)
+
+        queries = tf.convert_to_tensor(queries)
+        keys = tf.convert_to_tensor(keys)
+
+        module = MultiHeadAttention(
+            num_heads=2
+        )
+
+        out = module(queries, keys)
+        with self.test_session() as sess:
+            sess.run(tf.global_variables_initializer())
+            res = out.eval()
+
+            p = 1
